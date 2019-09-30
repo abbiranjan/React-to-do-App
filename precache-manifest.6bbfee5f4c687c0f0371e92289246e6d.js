f3629dce8c8c8a2a0d26793e0b8e10f300caaee8
Alok <abbiranjan@gmail.com>
2019-10-01 00:47:06 +0530

    Updates

---
+++ precache-manifest.6bbfee5f4c687c0f0371e92289246e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c119f336846eb62e35854b1869ee230",
+    "url": "/React-to-do-App/index.html"
+  },
+  {
+    "revision": "26759edee4905b6082a4",
+    "url": "/React-to-do-App/static/css/main.667d2a23.chunk.css"
+  },
+  {
+    "revision": "ba9bb9950dfa8f67abb8",
+    "url": "/React-to-do-App/static/js/2.17438d59.chunk.js"
+  },
+  {
+    "revision": "26759edee4905b6082a4",
+    "url": "/React-to-do-App/static/js/main.ca086235.chunk.js"
+  },
+  {
+    "revision": "fabdbdd368cc73152e85",
+    "url": "/React-to-do-App/static/js/runtime~main.64fddda7.js"
+  }
+]);
